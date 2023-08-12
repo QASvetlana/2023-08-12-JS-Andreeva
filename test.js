8fef8968bad3d96959cde8c5765b684bd82f06a9
Sveta Andreeva <89111378147v@yandex.com>
2023-08-12 15:45:12 +0300

    edit test

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log('Test')
+function greet (user) {
+    console.log('Test ' + user)    
+}
+
+const bye = (name) => {
+    console.log('Test2 ' + name)
+}
+greet('First user')
+bye('Second user')
