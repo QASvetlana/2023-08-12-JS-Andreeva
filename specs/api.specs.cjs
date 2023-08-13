6c877f3444e14689b560a4450555939069a1c188
Sveta Andreeva <89111378147v@yandex.com>
2023-08-13 08:59:24 +0300

    add specs

---
+++ specs/api.specs.cjs
@@ -0,0 +1,9 @@
+import request from 'supertest';
+
+test('Get request', async () => {
+    const URL = 'https://dummyjson.com/products';
+    const response = await fetch(URL);
+    const data = await  response.json();
+    console.log(data);
+    expect(response.status).toBe('200');
+})
