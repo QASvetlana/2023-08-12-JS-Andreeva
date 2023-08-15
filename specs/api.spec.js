525f7679cadc28b2f9a12350cc6ec2c611b2dae9
Sveta Andreeva <89111378147v@yandex.com>
2023-08-15 17:45:27 +0300

    tests for dummyjson api added

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 
+//Get all products
 test('Get request', async () => {
     const URL = 'https://dummyjson.com/products';
     // сохраняем ответ с запроса
@@ -9,6 +10,7 @@ test('Get request', async () => {
     expect(response.status).toBe(200);
 })
 
+//Add a new product
 test('Post request', async () => {
     const URL = 'https://dummyjson.com/products/add';
     const response = await fetch(URL, {
@@ -26,3 +28,35 @@ test('Post request', async () => {
     expect(data.id).toEqual(101);
 })
 
+//Get a single product
+test('Get request', async () => {
+    const URL = 'https://dummyjson.com/products/1';
+    const response = await fetch(URL);
+    const data = await  response.json();
+    console.log(data);
+    expect(data.id).toEqual(1);
+})
+
+//Search products
+test('Get request', async () => {
+    const URL = 'https://dummyjson.com/products/search?q=phone';
+    const response = await fetch(URL);
+    const data = await  response.json();
+    console.log(data);
+    expect(response.status).toBe(200);
+})
+
+//Update a product
+test('Post request', async () => {
+    const URL = 'https://dummyjson.com/products/1';
+    const response = await fetch(URL, {
+        method: 'PUT', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: 'iPhone Galaxy +1'
+        })
+      })
+      const data = await  response.json();
+      console.log(data);
+      expect(data.title).toEqual('iPhone Galaxy +1');
+    })
