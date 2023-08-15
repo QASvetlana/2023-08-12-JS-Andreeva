54971a5b082739e723eca89d244e111a1f57212e
Sveta Andreeva <89111378147v@yandex.com>
2023-08-15 15:18:19 +0300

    add test fot method POST

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -2,8 +2,27 @@ import request from 'supertest';
 
 test('Get request', async () => {
     const URL = 'https://dummyjson.com/products';
+    // сохраняем ответ с запроса
     const response = await fetch(URL);
     const data = await  response.json();
     console.log(data);
     expect(response.status).toBe(200);
 })
+
+test('Post request', async () => {
+    const URL = 'https://dummyjson.com/products/add';
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: 101,
+            title: 'BMW Pencil'
+        })
+    })
+    const data = await response.json();
+    console.log(data);
+    expect(data.id).toEqual(101);
+})
+
